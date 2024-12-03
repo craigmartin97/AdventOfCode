56db7ed20f11b682cfc725fa06468c396570fefd
craigmartin97 <c.martin0297@gmail.com>
2024-12-03 14:53:37 +0000

    complete day one part one

---
+++ AdventOfCode/AdventOfCode/2024/1/DayOne.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Immutable;
+
+namespace AdventOfCode._2024._1;
+
+public sealed class DayOne
+{
+    public int Execute(IEnumerable<int> listOne, IEnumerable<int> listTwo)
+    {
+        ArgumentNullException.ThrowIfNull(listOne, nameof(listOne));
+        ArgumentNullException.ThrowIfNull(listTwo, nameof(listTwo));
+
+        ImmutableArray<int> listOneArr = listOne.ToImmutableArray();
+        ImmutableArray<int> listTwoArr = listTwo.ToImmutableArray();
+
+        if (listOneArr.Length != listTwoArr.Length)
+        {
+            throw new InvalidOperationException("The two lists are not the same length");
+        }
+
+        listOneArr = listOneArr.Sort();
+        listTwoArr = listTwoArr.Sort();
+
+        int sum = 0;
+        for (int i = 0; i < listOneArr.Length; i++)
+        {
+            int smallestFromListOne = listOneArr[i];
+            int smallestFromListTwo = listTwoArr[i];
+            sum += Math.Abs(smallestFromListTwo - smallestFromListOne);
+        }
+
+        return sum;
+    }
+}
