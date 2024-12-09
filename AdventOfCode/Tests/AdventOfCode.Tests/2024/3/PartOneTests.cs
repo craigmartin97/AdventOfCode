58d166d6882d6e9a83f5dda7137e3b98d700c6cd
craigmartin97 <c.martin0297@gmail.com>
2024-12-09 18:28:59 +0000

    feat(daythree): add unit tests

---
+++ AdventOfCode/Tests/AdventOfCode.Tests/2024/3/PartOneTests.cs
@@ -0,0 +1,37 @@
+﻿using AdventOfCode._2024._3;
+using FluentAssertions;
+
+namespace AdventOfCode.UnitTests._2024._3;
+
+public class PartOneTests
+{
+    [Fact]
+    public void SumUsingStringSplitting_WithExampleData_Expected161()
+    {
+        // Arrange
+        PartOne part = new();
+
+        const string input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+        // Act
+        int sum = part.Sum_UsingStringSplitting(input);
+
+        // Assert
+        sum.Should().Be(161);
+    }
+
+    [Fact]
+    public void SumUsingRegex_WithExampleData_Expected161()
+    {
+        // Arrange
+        PartOne part = new();
+
+        const string input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+        // Act
+        int sum = part.Sum_UsingRegex(input);
+
+        // Assert
+        sum.Should().Be(161);
+    }
+}
